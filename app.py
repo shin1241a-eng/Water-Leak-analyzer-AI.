2075121d2405e5c159c7707d63aff59910aca318
shin1241a-eng <shin1241a@gmail.com>
2026-02-07 23:38:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,9 @@ def analyze():
 
     return jsonify({"result": result})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
