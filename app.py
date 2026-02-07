04548b8a201879cebd4220c4962ca8bff74eb026
shin1241a-eng <shin1241a@gmail.com>
2026-02-08 00:59:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import gdown
 from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
+
 CORS(app)
 
 # ================== โหลดโมเดลจาก Google Drive ==================
@@ -62,4 +63,5 @@ def analyze():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
 
